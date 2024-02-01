93b6f659cb1caa85fbfbdcc20875caae028a4bc0
sam-lo <54739761+sam-lo@users.noreply.github.com>
2024-02-01 23:04:06 +0800

    First Commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div>
+      <div className="relative">
         <Image className="brightness-125" src={heroIvan} alt={"heroIvan"}/>
+        <div className="absolute left-8 bottom-8 md:inset-y-8 transition-all duration-1000">
+          <div className="backdrop-blur hover:scale-105 transition-all duration-1000 p-4 md:p-8 rounded-2xl">
+            <div className="text-4xl font-bold">
+              Join our workshop!
+            </div>
+            <div>
+              to start your journey in Handflute
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
