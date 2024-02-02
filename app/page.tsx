1103e24d7f2f5301d3bc5359be69a33d2c703ea2
sam-lo <54739761+sam-lo@users.noreply.github.com>
2024-02-02 11:23:15 +0800

    add nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
     <div>
       <div className="relative">
         <Image className="brightness-125" src={heroIvan} alt={"heroIvan"}/>
-        <div className="absolute left-8 bottom-8 md:inset-y-8 transition-all duration-1000">
-          <div className="backdrop-blur hover:scale-105 transition-all duration-1000 p-4 md:p-8 rounded-2xl">
+        <div className="absolute left-8 bottom-8 lg:inset-y-8 transition-all duration-1000">
+          <div className="backdrop-blur hover:scale-105 transition-all duration-1000 p-4 lg:p-8 rounded-2xl">
             <div className="text-4xl font-bold">
               Join our workshop!
             </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,38 @@
-import logo from "@/public/Logo_MainTheme_wText.jpg"
+import logo from "@/public/Logo_MainTheme_wText.jpg";
+import { MagnifyingGlassIcon,  LanguageIcon, Bars3Icon } from "@heroicons/react/24/solid";
+import { Fragment } from 'react'
 import Image from "next/image";
+
 export default function Header() {
   return (
-    <div className="p-4 bg-orange-50 flex items-center space-x-8">
-      <Image className="size-16 mx-4 rounded-2xl shadow-2xl hover:scale-110 transition-all duration-500" src={logo}
-             alt={"logo"}/>
-      <div className="text-xl font-bold text-yellow-700">
-        About Us
-      </div>
-      <div className="text-xl font-bold text-yellow-700">
-        Our Work
-      </div>
-      <div className="text-xl font-bold text-yellow-700">
-        Resources
-      </div>
-      <div className="text-xl font-bold text-yellow-700">
-        Get Involved
+    <div>
+      <div className="divide-y divide-orange-200">
+        <div className="p-4 bg-orange-50 flex items-center justify-between">
+          <Image className="size-16 mx-4 rounded-2xl shadow-2xl hover:scale-110 transition-all duration-500" src={logo}
+                 alt={"logo"}/>
+          <div className="flex items-center">
+            <MagnifyingGlassIcon className="size-6 fill-yellow-700 stroke-yellow-700 mx-4"/>
+            <LanguageIcon className="size-6 fill-yellow-700 stroke-yellow-700 mx-4"/>
+            <Bars3Icon className="size-6 fill-yellow-700 stroke-yellow-700 mx-4" />
+          </div>
+        </div>
+        <div
+          className="py-2 hidden lg:flex text-xl font-bold text-yellow-700 items-center justify-evenly bg-orange-50">
+          <div>
+            About Us
+          </div>
+          <div>
+            Our Work
+          </div>
+          <div>
+            Resources
+          </div>
+          <div>
+            Get Involved
+          </div>
+        </div>
       </div>
     </div>
-  );
+  )
+    ;
 }
